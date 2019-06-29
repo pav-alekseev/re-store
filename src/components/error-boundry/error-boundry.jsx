1717060d5e38975eb2687525ce61f091ac4b1e23
RukkiesMan <pavelekseev@gmail.com>
2019-06-29 14:49:18 +0300

    Remove css from ErrorBoundry component

--- src/components/error-boundry/error-boundry.jsx
+++ src/components/error-boundry/error-boundry.jsx
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-import './error-boundry.css';
-
 import ErrorIndicator from '../error-indicator';
 
 export default class ErrorBoundry extends Component {
