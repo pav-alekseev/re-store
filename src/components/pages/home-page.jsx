31035e5bda0dcf22eacb9a899f39320d71ef67cc
RukkiesMan <pavelekseev@gmail.com>
2019-06-29 20:39:46 +0300

    Render list of example books on the HomePage

--- src/components/pages/home-page.jsx
+++ src/components/pages/home-page.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import { withRouter } from 'react-router-dom';
 
+import BookList from '../book-list';
+
 const HomePage = () => (
-  <div>
-    <p>Home Page</p>
-  </div>
+  <BookList books={[
+    {
+      id: 1,
+      title: 'Production-Ready Microservies',
+      author: 'Susan J. Fowler',
+    },
+    {
+      id: 2,
+      title: 'Release It!',
+      author: 'Michael T. Nygard',
+    },
+  ]}
+  />
 );
 
 export default withRouter(HomePage);
