ac57af43886355af226ccf8244c319eab98cd496
RukkiesMan <pavelekseev@gmail.com>
2019-06-29 16:50:34 +0300

    Add test data for books

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -1,5 +1,16 @@
 export default class BookstoreService {
   getBooks() {
-    return [];
+    return [
+      {
+        id: 1,
+        title: 'Production-Ready Microservies',
+        author: 'Susan J. Fowler',
+      },
+      {
+        id: 2,
+        title: 'Release It!',
+        author: 'Michael T. Nygard',
+      },
+    ];
   }
 }
