c12571978dc190920c01f9ac5eff79818344b8fa
RukkiesMan <pavelekseev@gmail.com>
2019-06-29 09:02:17 +0300

    Add Spinner component

--- src/components/spinner/spinner.jsx
+++ src/components/spinner/spinner.jsx
@@ -2,6 +2,23 @@ import React from 'react';
 
 import './spinner.css';
 
-const Spinner = () => {};
+const Spinner = () => (
+  <div className="lds-css ng-scope">
+    <div className="lds-spinner" style={{ width: '100%', height: '100%' }}>
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+      <div />
+    </div>
+  </div>
+);
 
 export default Spinner;
