5948a58e575acd88eac2cbf18d9035fa4ef565c0
RukkiesMan <pavelekseev@gmail.com>
2019-06-29 20:38:57 +0300

    Add BookListItem component

---
+++ src/components/book-list-item/book-list-item.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react';
+
+import './book-list-item.css';
+
+const BookListItem = ({ book }) => {
+  const { title, author } = book;
+  return (
+    <Fragment>
+      <span>{title}</span>
+      <span>{author}</span>
+    </Fragment>
+  );
+};
+
+export default BookListItem;
