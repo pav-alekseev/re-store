13c619dc89703ad02d98e5cd5b587cef64607e7b
RukkiesMan <pavelekseev@gmail.com>
2019-06-29 09:25:18 +0300

    Add ErrorBoundry component

--- src/components/error-boundry/error-boundry.jsx
+++ src/components/error-boundry/error-boundry.jsx
@@ -1,7 +1,37 @@
-import React from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 import './error-boundry.css';
 
-const ErrorBoundry = () => {};
+import ErrorIndicator from '../error-indicator';
 
-export default ErrorBoundry;
+export default class ErrorBoundry extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      hasError: false,
+    };
+  }
+
+  componentDidCatch() {
+    this.setState({
+      hasError: true,
+    });
+  }
+
+  render() {
+    const { children } = this.props;
+    const { hasError } = this.state;
+
+    if (hasError) {
+      return <ErrorIndicator />;
+    }
+
+    return children;
+  }
+}
+
+ErrorBoundry.propTypes = {
+  children: PropTypes.node.isRequired,
+};
