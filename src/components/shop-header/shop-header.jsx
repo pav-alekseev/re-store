360eb6284d0b2d19a3fc1a47e5ee7583c3c5b9b9
RukkiesMan <pavelekseev@gmail.com>
2019-06-30 10:32:02 +0300

    Style ShopHeader component

--- src/components/shop-header/shop-header.jsx
+++ src/components/shop-header/shop-header.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 import './shop-header.css';
 
 const ShopHeader = ({ numItems, total}) => (
-  <header>
-    <a href="./"> ReStore</a>
-    <a href="./">
-      <i className="fa fa-shopping-cart" />
+  <header className="shop-header row">
+    <a className="logo text-dark" href="./"> ReStore</a>
+    <a className="shopping-cart" href="./">
+      <i className="cart-icon fa fa-shopping-cart" />
       {numItems} items (${total})
     </a>
   </header>
