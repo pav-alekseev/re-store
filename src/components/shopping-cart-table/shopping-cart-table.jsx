94d8a1bd5c4d788a4a22fc62a4cd8c5f1dcc0efc
RukkiesMan <pavelekseev@gmail.com>
2019-06-30 12:52:42 +0300

    Connect ShoppingCartTable to Redux

--- src/components/shopping-cart-table/shopping-cart-table.jsx
+++ src/components/shopping-cart-table/shopping-cart-table.jsx
@@ -1,45 +1,91 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
 import './shopping-cart-table.css';
 
-const ShoppingCartTable = () => (
-  <div className="shopping-cart-table">
-    <h2>Your Order</h2>
-    <table className="table">
-      <thead>
-        <tr>
-          <th>#</th>
-          <th>Item</th>
-          <th>Count</th>
-          <th>Price</th>
-          <th>Action</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>1</td>
-          <td>Site Reliablity Engineering</td>
-          <td>2</td>
-          <td>$40</td>
-          <td>
-            <button className="btn btn-outline-danger btn-sm float-right" type="button">
-              <i className="fa fa-trash-o" />
-            </button>
-            <button className="btn btn-outline-success btn-sm float-right" type="button">
-              <i className="fa fa-plus-circle" />
-            </button>
-            <button className="btn btn-outline-warning btn-sm float-right" type="button">
-              <i className="fa fa-minus-circle" />
-            </button>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+const ShoppingCartTable = ({
+  items, total, onIncrease, onDecrease, onDelete
+}) => {
+  const renderRow = (item, idx) => {
+    const {
+      id, name, count, total,
+    } = item;
 
-    <div className="total">
-      Total: $201
+    return (
+      <tr key={id}>
+        <td>{idx + 1}</td>
+        <td>{name}</td>
+        <td>{count}</td>
+        <td>${total}</td>
+        <td>
+          <button
+            className="btn btn-outline-danger btn-sm float-right"
+            type="button"
+            onClick={() => onDelete(id)}
+          >
+            <i className="fa fa-trash-o" />
+          </button>
+          <button
+            className="btn btn-outline-success btn-sm float-right"
+            type="button"
+            onClick={() => onIncrease(id)}
+          >
+            <i className="fa fa-plus-circle" />
+          </button>
+          <button
+            className="btn btn-outline-warning btn-sm float-right"
+            type="button"
+            onClick={() => onDecrease(id)}
+          >
+            <i className="fa fa-minus-circle" />
+          </button>
+        </td>
+      </tr>
+    );
+  };
+
+  return (
+    <div className="shopping-cart-table">
+      <h2>Your Order</h2>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Item</th>
+            <th>Count</th>
+            <th>Price</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            items.map(renderRow)
+          }
+        </tbody>
+      </table>
+
+      <div className="total">
+        Total: ${total}
+      </div>
     </div>
-  </div>
-);
+  );
+};
 
-export default ShoppingCartTable;
+const mapStateToProps = ({ cartItems, orderTotal }) => ({
+  items: cartItems,
+  total: orderTotal,
+});
+
+const mapDispatchToProps = () => ({
+  onIncrease: (id) => {
+    console.log(`Increase ${id}`);
+  },
+  onDecrease: (id) => {
+    console.log(`Decrease ${id}`);
+  },
+  onDelete: (id) => {
+    console.log(`Delete ${id}`);
+  },
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(ShoppingCartTable);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -4,24 +4,42 @@ const initialState = {
   books: [],
   loading: true,
   error: null,
+  cartItems: [
+    {
+      id: 1,
+      name: 'Book 1',
+      count: 3,
+      total: 150,
+    },
+    {
+      id: 2,
+      name: 'Book 2',
+      count: 2,
+      total: 70,
+    },
+  ],
+  orderTotal: 220,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.FETCH_BOOKS_REQUEST:
       return {
+        ...state,
         books: [],
         loading: true,
         error: null,
       };
     case actionTypes.FETCH_BOOKS_SUCCESS:
       return {
+        ...state,
         books: action.payload,
         loading: false,
         error: null,
       };
     case actionTypes.FETCH_BOOKS_FAILURE:
       return {
+        ...state,
         books: [],
         loading: false,
         error: action.payload,
