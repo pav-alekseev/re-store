29d3798fc5bd38e2b120f139e936ac396f296192
RukkiesMan <pavelekseev@gmail.com>
2019-06-30 10:53:50 +0300

    Change link to span in the BookListItem component

--- src/components/book-list-item/book-list-item.jsx
+++ src/components/book-list-item/book-list-item.jsx
@@ -12,7 +12,7 @@ const BookListItem = ({ book }) => {
         <img src={coverImage} alt="cover" />
       </div>
       <div className="book-details">
-        <a href="./" className="book-title">{title}</a>
+        <span className="book-title">{title}</span>
         <div className="book-author">{author}</div>
         <div className="book-price">{price}</div>
         <button className="btn btn-info add-to-cart" type="button">Add to cart</button>
