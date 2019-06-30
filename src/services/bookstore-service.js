09123dc96092a6a6de957f454335091e2cd7a3ca
RukkiesMan <pavelekseev@gmail.com>
2019-06-30 10:27:43 +0300

    Add additional data to books in the BookstoreService

--- src/services/bookstore-service.js
+++ src/services/bookstore-service.js
@@ -5,11 +5,15 @@ export default class BookstoreService {
         id: 1,
         title: 'Production-Ready Microservies',
         author: 'Susan J. Fowler',
+        price: 32,
+        coverImage: 'https://images-na.ssl-images-amazon.com/images/I/41yJ75gpV-L._SX381_BO1,204,203,200_.jpg',
       },
       {
         id: 2,
         title: 'Release It!',
         author: 'Michael T. Nygard',
+        price: 45,
+        coverImage: 'https://images-na.ssl-images-amazon.com/images/I/414CRjLjwgL._SX403_BO1,204,203,200_.jpg',
       },
     ];
   }
