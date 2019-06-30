223e31ba31dd03b4a932de953ac4d4a388e59261
RukkiesMan <pavelekseev@gmail.com>
2019-06-30 10:28:16 +0300

    Style BookListItem component

--- src/components/book-list-item/book-list-item.jsx
+++ src/components/book-list-item/book-list-item.jsx
@@ -1,14 +1,23 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 
 import './book-list-item.css';
 
 const BookListItem = ({ book }) => {
-  const { title, author } = book;
+  const {
+    title, author, price, coverImage,
+  } = book;
   return (
-    <Fragment>
-      <span>{title}</span>
-      <span>{author}</span>
-    </Fragment>
+    <div className="book-list-item">
+      <div className="book-cover">
+        <img src={coverImage} alt="cover" />
+      </div>
+      <div className="book-details">
+        <a href="./" className="book-title">{title}</a>
+        <div className="book-author">{author}</div>
+        <div className="book-price">{price}</div>
+        <button className="btn btn-info add-to-cart" type="button">Add to cart</button>
+      </div>
+    </div>
   );
 };
 
