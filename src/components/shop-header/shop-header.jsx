7e248e85165391389237d38ec3135bf8d2977c09
RukkiesMan <pavelekseev@gmail.com>
2019-06-30 10:53:20 +0300

    Use Link to create links in the Header component

--- src/components/shop-header/shop-header.jsx
+++ src/components/shop-header/shop-header.jsx
@@ -1,15 +1,20 @@
 /* eslint-disable react/jsx-one-expression-per-line */
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import './shop-header.css';
 
 const ShopHeader = ({ numItems, total}) => (
   <header className="shop-header row">
-    <a className="logo text-dark" href="./"> ReStore</a>
-    <a className="shopping-cart" href="./">
-      <i className="cart-icon fa fa-shopping-cart" />
-      {numItems} items (${total})
-    </a>
+    <Link to="/">
+      <div className="logo text-dark"> ReStore</div>
+    </Link>
+    <Link to="/cart">
+      <div className="shopping-cart">
+        <i className="cart-icon fa fa-shopping-cart" />
+        {numItems} items (${total})
+      </div>
+    </Link>
   </header>
 );
 
